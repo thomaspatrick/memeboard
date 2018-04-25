058831630a13ab5c93adf69e421c1fb9ed57b7a6
Thomas Patrick <thomas.patrick@easeinc.com>
2018-04-24 21:55:37 -0700

    meme board voice activation

--- src/main.js
+++ src/main.js
@@ -14,11 +14,54 @@ function buildCard(key) {
     $('#'+key + ' a').on('click',function() { onCardClick(key); });
 }
 
+function normalizeString(str) {
+    return str.toLowerCase().replace(/[^a-z0-9]/ig, '')
+}
 
-$(document).ready(function() {
-    $.getJSON('config/items.json',function(data) {
-        items = data;
-        $.each(items,buildCard);
+function playIfMatch(phrase) {
+    var matched = false;
+    var normalizedPhrase = normalizeString(phrase);
+    $.each(items, function (key) {
+        var normalizedKey = normalizeString(key);
+        if(normalizedPhrase.includes(normalizedKey)) {
+            matched = true;
+            console.log("voice recognition match for " + normalizedKey);
+            $('#'+key + ' a').click().fadeTo(100, .3, function() { $(this).fadeTo(500, 1.0); });;
+        }
     });
-}) ;
+
+    if(!matched) {
+        console.log("no voice recognition match for " + normalizedPhrase);
+    }
+}
+
+var rec = null;
+var items = null;
+$(document).ready(function() {
+    $.getJSON('config/items.json', function (data) {
+        items = data;
+        $.each(items, buildCard);
+    });
+
+    if ('webkitSpeechRecognition' in window) {
+        rec = new webkitSpeechRecognition();
+        rec.lang = 'en-US';
+        rec.interimResults = false;
+        rec.continuous = true;
+        rec.maxAlternatives = 1;
+        rec.start();
+        rec.onspeechend = function () {
+            rec.stop();
+            window.setTimeout(function () {
+                rec.start();
+            }, 1000);
+        }
+        rec.onresult = function (event) {
+            var last = event.results.length - 1;
+            var spoken = event.results[last][0].transcript;
+            playIfMatch(spoken);
+        }
+    }
+});
+
 
